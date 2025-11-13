20667d86d92b08a3bb0ea28295bf28fab5b57e46
garell1 <galkowski.mateusz@outlook.com>
2025-11-13 22:29:52 +0100

    add templates

--- meetups/urls.py
+++ meetups/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('meetups/', views.starting_pager),
-    path('meetups/<slug:meetup_slug>', views.meetup_details)
+    path('meetups/', views.starting_pager, name='all-meetups'),
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail')
 ]
