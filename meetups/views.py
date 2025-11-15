9cd5d5bb6e6f93f664197fd1c6384f5347d06431
garell1 <galkowski.mateusz@outlook.com>
2025-11-15 22:01:57 +0100

    add database

--- meetups/views.py
+++ meetups/views.py
@@ -1,31 +1,25 @@
 from django.shortcuts import render
 # Create your views here.
 
+from .models import Meetup
+
 def starting_pager(request):
-    meetups = [
-        {
-            'title': "A First meetup", 
-            'location': 'New Your', 
-            'slug': 'a-first-meetup'
-        },
-        {
-            'title': "A Second meetup", 
-            'location': 'Paris', 
-            'slug': 'a-second-meetup'
-        }
-    ]
+    meetups = Meetup.objects.all()
     return render(request,'meetups/index.html',{
         'show_meetups': True,
         'meetups': meetups
     })
 
 def meetup_details(request, meetup_slug):
-    print(meetup_slug)
-    selected_meetup = {
-        'title': 'A First Meetup', 
-        'description': 'This is first meetup'
-        }
-    return render(request, 'meetups/meetup_detail.html', {
-        'meetup_title': selected_meetup['title'],
-        'meetup_description': selected_meetup['description'],
-    })
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(request, 'meetups/meetup_detail.html', {
+            'meetup_found': True,
+            'meetup_title': selected_meetup.title,
+            'meetup_description': selected_meetup.description,
+        })
+    except Exception as exc:
+        return render(request,'meetups/meetup_detail.html', {
+            'meetup_found': False,
+            
+        })
